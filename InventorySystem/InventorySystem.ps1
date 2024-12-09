32f7357e2c938f5ee60d8ee4373c8b7ca58f1951
Cédric Z <cedriczapart@gmail.com>
2024-12-09 19:34:22 +0100

    Amelioration structure of script

--- InventorySystem/InventorySystem.ps1
+++ InventorySystem/InventorySystem.ps1
@@ -1,25 +1,57 @@
+#******************************************************************************************************************************
+#                                                                                                                             *
+# file : InventorySystem.ps1                                                                                                  *
+#                                                                                                                             *
+# Version : 1.0                                                                                                               *
+#                                                                                                                             *
+# Date : 09/12/2024                                                                                                           *
+#                                                                                                                             *
+# Description : pull inventory system Name Bios, Version of Bios, Name CPU, Socket CPU, NamePC,DomainName and model computer  *
+#                                                                                                                             *
+#******************************************************************************************************************************
+
+# ======================================================================================
+#  VARIABLE DECLARATIONS                                                               
+#=======================================================================================
+#
+# code exit initial
+$ExitCode = 0
+
+
+# #check if Directory is not present \COPY AND PASTE, YOUR LOGFILE
+$LogDirectory = "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\InventorySystem\"
+
+# Path LogFile \ MODIFY THE PATH TO SUIT  FOR YOUR NEED
+$logfilepath = "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\InventorySystem\LogFile.txt"
+
+
+
+#==================================================================================================================
+# FUNCTION DECLARATION
+#==================================================================================================================
+#
+
+#
+# this function write-log, write informations of du script  in $logfilepath and $LogDirectory with date and hours 
 function Write-log {
     param(
         [string]$Message,
         [string]$Event
     )
-    # Path logs 
-    $logfilepath = "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\InventorySystem\LogFile.txt"
-    
-    #check if path log is not present
-    if (!(Test-Path -Path "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\InventorySystem")) {
-        New-Item -ItemType Directory -Path "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\InventorySystem" -Force
+
+    if (!(Test-Path -Path $LogDirectory)) {
+        New-Item -ItemType Directory -Path $LogDirectory -Force
     }
     if (!(Test-Path -Path $logfilepath)) {
         New-Item -ItemType File -Path $logfilepath -Force
     }
 
-    # Ajoute le message au fichier
+    # add a message and event in your log
     $timestamp = Get-Date -Format "dd/MM/yyyy-HH:mm:ss"
     Add-Content -Path $logfilepath -Value "[$timestamp][$Event] $Message"
 }
 
-
+# this function retrive information system computer name,domaine name, name bios, version bios,name processor, socket processor and write informations in log with function write log
 function InventorySystem () {
 try {
     
@@ -52,4 +84,13 @@ try {
   }
 }
 
+#==================================================================================================================
+# MAIN 
+#==================================================================================================================
+
 InventorySystem
+exit $ExitCode++
+
+#======================================================================================================================
+# END OF SCRIPT
+#======================================================================================================================
