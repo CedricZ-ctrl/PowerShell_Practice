0d134f348b3d903a50294096b095daac6f062370
Cédric Z <cedriczapart@gmail.com>
2024-12-19 21:41:59 +0100

    add script backup KeyRegistry

---
+++ RegistryBackup/RegistryBackup.ps1
@@ -0,0 +1,110 @@
+#******************************************************************************************************************************
+#                                                                                                                             *
+# file : RegistryBackup.ps1                                                                                                  *
+#                                                                                                                             *
+# Version : 1.0                                                                                                               *
+#                                                                                                                             *
+# Date : 18/12/2024                                                                                                           *
+#                                                                                                                             *
+# Description : Th*
+#                                                                                                                             *
+#******************************************************************************************************************************
+
+# ======================================================================================
+#  VARIABLE DECLARATIONS                                                               
+#=======================================================================================
+#
+# code exit initial
+$ExitCode = 0
+
+# #check if Directory is not present \COPY AND PASTE, YOUR LOGFILE
+$LogDirectory = "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\RegistryBackup\"
+
+# Path LogFile \ MODIFY THE PATH TO SUIT  FOR YOUR NEED
+$logfilepath = "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\RegistryBackup\LogFile.txt"
+
+#path backup registry 
+$RegFile = "B:\VSCode_Exercice\Exercices_Powershell\PowerShell_Practice\RegistryBackup\Notepad.reg"
+
+#==================================================================================================================
+# FUNCTION DECLARATION
+#==================================================================================================================
+#
+
+# this function write-log, write informations of du script  in $logfilepath and $LogDirectory with date and hours 
+function Write-log {
+    param(
+        [string]$Message,
+        [string]$Event
+    )
+
+    if (!(Test-Path -Path $LogDirectory)) {
+        New-Item -ItemType Directory -Path $LogDirectory -Force
+    }
+    if (!(Test-Path -Path $logfilepath)) {
+        New-Item -ItemType File -Path $logfilepath -Force
+    }
+
+    # add a message and event in your log
+    $timestamp = Get-Date -Format "dd/MM/yyyy-HH:mm:ss"
+    Add-Content -Path $logfilepath -Value "[$timestamp][$Event] $Message"
+}
+
+
+function ExportReg {
+    try {
+        if (!(Test-Path -Path $RegFile)) {
+        reg export HKLM\SOFTWARE\Microsoft\Notepad $RegFile
+        $Message ="the backup of the key registry $RegFile is create"
+        Write-log -Event "INFO" -Message $Message
+    }
+    else {
+        $Message ="Backup Registry already present"
+        Write-log -Event "INFO" -Message $Message
+    }
+}
+ catch {
+        $ExitCode = 1
+        $Message ="an error occured in creating $RegFile : $($ExitCode): $_"
+        Write-log -Event "WARNING" -Message $Message
+        }
+    }
+
+
+
+function ImportReg {
+    try {
+        if (Test-Path -Path $RegFile) {
+           reg import $RegFile
+            $Message = "Registry restored from : $RegFile"
+            Write-log -Event "INFO" -Message $Message
+        }
+        else {
+            $Message ="Backup File registry not found in: $RegFile "
+            Write-log -Event "ERROR" -Message $Message
+        }
+    }
+    catch {
+        $ExitCode = 2
+        $Message ="An error occurred in restoring the registry : $($ExitCode): $_ "
+        Write-log -Event "WARNING" -Message $Message
+    } 
+}
+
+$Choice = Read-Host "What do you want ? Import or Export ?"
+
+
+switch ($Choice)
+ {
+    "Export" { ExportReg }
+    "Import" { ImportReg }
+    Default { Write-Host " invalid choice, exiting script"}
+}
+#==================================================================================================================
+# MAIN 
+#==================================================================================================================
+
+
+#======================================================================================================================
+# END OF SCRIPT
+#======================================================================================================================
